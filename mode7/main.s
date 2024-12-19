41a9cec6a9cf7325f8600d32cb1e9b62a96421e3
Taylor Plewe <tplewe@outlook.com>
2024-12-19 13:16:23 -0700

    rotate counter-clockwise

--- mode7/main.s
+++ mode7/main.s
@@ -7,6 +7,7 @@ joy1_prev:		.res 2
 joy1_pressed:	.res 2
 
 m7_inc:			.res 2
+m7_dec:			.res 2
 
 	.code
 reset:
@@ -39,9 +40,18 @@ reset:
 	lda #$f
 	sta INIDISP
 
+	; set m7_dec to 1.0
+	lda #1
+	sta m7_dec+1
+
+	; m7 empty space = bg, don't loop
+	; lda #%10000000
+	; sta M7SEL
+
 forever:
 	a16
 	inc m7_inc
+	dec m7_dec
 	a8
 	jsr wait_for_input
 
@@ -58,91 +68,205 @@ nmi:
 	phx
 	phy
 
-	sta BG1HOFS
+	inc_cos = local
+	inc_cos_neg = local+2
+	inc_sin = local+4
+	inc_sin_neg = local+6
+
+	lda m7_inc
+	jsr cos
+	a16
+	jsr asr8
+	sta inc_cos
+	eor #$ffff
+	inc a
+	sta inc_cos_neg
+	a8
+	lda m7_inc
+	jsr sin
+	a16
+	jsr asr8
+	sta inc_sin
+	eor #$ffff
+	inc a
+	sta inc_sin_neg
+	a8
+
+
+	; increasing
+	; lda m7_inc
+	; sta BG1HOFS
+	; lda m7_inc+1
+	; sta BG1HOFS
+	; uniform
+	stz BG1HOFS
 	stz BG1HOFS
 	stz BG1VOFS
 	stz BG1VOFS
 
-	a16
-	i8
-	ldy #0
-	ldx #1
-	lda JOY1L
-	bit #JOY_A
-	beq :+
-		sty M7A
-		sty M7A
-		bra :++
-	:
+	; A
 		; uniform
-		; sty M7A
-		; stx M7A
+		stz M7A
+		lda #1
+		sta M7A
 
 		; increasing
-		ldy m7_inc
-		sty M7A
-		ldy m7_inc+1
-		sty M7A
-		ldy #0
+		; ldy m7_inc
+		; sty M7A
+		; ldy m7_inc+1
+		; sty M7A
+		; ldy #0
+
+		; decreasing
+		; ldy m7_dec
+		; sty M7A
+		; ldy m7_dec+1
+		; sty M7A
+		; ldy #0
 
 		; 2.0
 		; sty M7A
 		; ldy #2
 		; sty M7A
 		; ldy #0
-	:
-	bit #JOY_B
-	beq :+
-		sty M7B
-		sty M7B
-		bra :++
-	:
-		sty M7B
-		sty M7B
-		; ldy m7_inc
-		; sty M7B
-		; ldy m7_inc+1
-		; sty M7B
+
+		; 0.5
+		; ldy #$80
+		; sty M7A
 		; ldy #0
-	:
-	bit #JOY_X
-	beq :+
-		sty M7C
-		sty M7C
-		bra :++
-	:
-		; ldy m7_inc
-		; sty M7C
-		; ldy m7_inc+1
-		; sty M7C
-		; ldy #0
-		sty M7C
-		sty M7C
-	:
-	bit #JOY_Y
-	beq :+
-		sty M7D
-		sty M7D
-		bra :++
-	:
+		; sty M7A
+
+		; cos
+		lda inc_cos
+		sta M7A
+		lda inc_cos+1
+		sta M7A
+	; B
 		; uniform
-		; sty M7D
-		; stx M7D
+		stz M7B
+		stz M7B
 
 		; increasing
-		ldy m7_inc
-		sty M7D
-		ldy m7_inc+1
-		sty M7D
-		ldy #0
+		; ldy m7_inc
+		; sty M7B
+		; ldy m7_inc+1
+		; sty M7B
+		; ldy #0
+
+		; decreasing
+		; ldy m7_dec
+		; sty M7B
+		; ldy m7_dec+1
+		; sty M7B
+		; ldy #0
+
+		; 2.0
+		; sty M7B
+		; ldy #2
+		; sty M7B
+
+		; 0.5
+		; ldy #$80
+		; sty M7B
+		; ldy #0
+		; sty M7B
+
+		; sin
+		; lda m7_inc
+		; jsr sin
+		; xba
+		; sta M7B
+		; stz M7B
+		
+		; -sin
+		lda inc_sin_neg
+		sta M7B
+		lda inc_sin_neg+1
+		sta M7B
+	; C
+		; uniform
+		stz M7C
+		stz M7C
+
+		; increasing
+		; ldy m7_inc
+		; sty M7C
+		; ldy m7_inc+1
+		; sty M7C
+		; ldy #0
+
+		; decreasing
+		; ldy m7_dec
+		; sty M7C
+		; ldy m7_dec+1
+		; sty M7C
+		; ldy #0
+
+		; 2.0
+		; sty M7C
+		; ldy #2
+		; sty M7C
+
+		; 0.5
+		; ldy #$80
+		; sty M7C
+		; ldy #0
+		; sty M7C
+		
+		; sin
+		lda inc_sin
+		sta M7C
+		lda inc_sin+1
+		sta M7C
+		
+		; -sin
+		; lda m7_inc
+		; jsr sin
+		; xba
+		; eor #$ff
+		; inc a
+		; sta M7C
+		; lda #$ff
+		; sta M7C
+	; D
+		; uniform
+		stz M7D
+		lda #1
+		sta M7D
+
+		; increasing
+		; ldy m7_inc
+		; sty M7D
+		; ldy m7_inc+1
+		; sty M7D
+		; ldy #0
+
+		; decreasing
+		; ldy m7_dec
+		; sty M7D
+		; ldy m7_dec+1
+		; sty M7D
+		; ldy #0
 
 		; 2.0
 		; sty M7D
 		; ldy #2
 		; sty M7D
-	:
-	a8
-	i16
+
+		; 0.5
+		; ldy #$80
+		; sty M7D
+		; ldy #0
+		; sty M7D
+		
+		; cos
+		lda inc_cos
+		sta M7D
+		lda inc_cos+1
+		sta M7D
+	; :
+	; a8
+	; i16
 
 	; lda m7_inc
 	lda #$80
--- mode7/src/common.s
+++ mode7/src/common.s
@@ -63,3 +63,67 @@ set_joy1_pressed:
 	sta joy1_pressed
 	a8
 	rts
+
+; Come into this function A.16
+.a16
+asr8:
+	bit #$8000
+	beq @pos
+	;neg
+		xba
+		ora #$ff00
+		rts
+	@pos:
+		xba
+		and #$00ff
+		rts
+.a8
+
+; get COS(A.8) in A.16
+cos:
+	clc                    ; clear carry for add
+	adc  #$40              ; add 1/4 rotation
+
+; get SIN(A.8) in A.16. enter with the Z flag reflecting the contents of A
+sin:
+	bpl @sin_cos		; just get SIN/COS and return if +ve
+
+	and #$7f		; else make +ve
+	jsr @sin_cos		; get SIN/COS
+	
+	; now do twos complement
+	a16
+	eor #$ffff
+	clc
+	adc #1
+	a8
+	rts
+
+	; get 16-bit A from SIN/COS table
+	@sin_cos:
+	cmp #$41		; compare with max+1
+	bcc @quadrant	; branch if less
+
+	eor #$7F		; wrap $41 to $7F ..
+	adc #$00		; .. to $3F to $00
+
+	@quadrant:
+	asl	a			; * 2 bytes per value
+	i8
+	a16
+	tax				; copy to index
+	lda sintab, x	; get 16-bit SIN/COS table value
+	i16
+	a8
+	rts
+
+sintab:
+	.word $0000,$0324,$0647,$096A,$0C8B,$0FAB,$12C8,$15E2
+	.word $18F8,$1C0B,$1F19,$2223,$2528,$2826,$2B1F,$2E11
+	.word $30FB,$33DE,$36BE,$398C,$3C56,$3F17,$41CE,$447A
+	.word $471C,$49B4,$4C3F,$4EBF,$5133,$539B,$55F5,$5842
+	.word $5A82,$5CB4,$5ED7,$60EC,$62F2,$64EB,$66CF,$68A6
+	.word $6A6D,$6C24,$6DC4,$6F5F,$70E2,$7255,$73B5,$7504
+	.word $7641,$776C,$7884,$798A,$7A7D,$7B5D,$7C2A,$7CE3
+	.word $7D8A,$7E1D,$7E9D,$7F09,$7F62,$7FA7,$7FD8,$7FF6
+	.word $7FFF
